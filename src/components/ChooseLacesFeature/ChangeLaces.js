d088fe9a1d6ce6bc7ae325abd13385b97ed4a710
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-12 18:10:16 +0200

    Add Shoe Card with image

--- src/components/ChooseLacesFeature/ChangeLaces.js
+++ src/components/ChooseLacesFeature/ChangeLaces.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import ShoeCard from './ShoeCard';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +22,7 @@ function ChangeLaces() {
     <div className={classes.root}>
       <Grid container justify='center' alignItems='stretch' spacing={3}>
         <Grid item xs={12} sm={6} md={6} lg={6}>
-          <Paper className={classes.paper}>Big shoe card</Paper>
+          <ShoeCard />
         </Grid>
         <Grid item xs={12} sm={6} md={6} lg={6}>
           <Paper className={classes.paper}>Shoe slider</Paper>
