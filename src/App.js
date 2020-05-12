3b1c322b1ac14eec75336e57a419fa75e8f96ff4
Maja Gach <majaanna94@gmail.com>
2020-05-12 17:36:27 +0200

    slider component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+
+// components
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import DisplayCustomShoes from './components/ChooseLacesFeature/DisplayCustomShoes';
+
 function App() {
   return (
     <div className="App">
       <div className="content">
         <NavBar />
+
         <DisplayCustomShoes />
       </div>
       <Footer />
