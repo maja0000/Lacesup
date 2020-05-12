34669d7d6042caf23d200b6052894125560b78e6
julia.natsuko@gmail.com <julia.natsuko@gmail.com>
2020-05-12 17:10:55 +0200

    Change grid

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import DisplayCustomShoes from './components/ChooseLacesFeature/DisplayCustomShoes';
+import Grid from '@material-ui/core/Grid';
+
 function App() {
   return (
     <div>
       <NavBar />
-      <div className='content'>
-        <NavBar />
-      </div>
+      <div className='content'></div>
       <DisplayCustomShoes />
       <Footer />
     </div>
--- src/components/ChooseLacesFeature/ChangeLaces.js
+++ src/components/ChooseLacesFeature/ChangeLaces.js
@@ -19,20 +19,20 @@ function ChangeLaces() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={3}>
+      <Grid container justify='center' alignItems='stretch' spacing={3}>
+        <Grid item xs={12} sm={6} md={6} lg={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={6} md={6} lg={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} md={6} lg={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} md={6} lg={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} md={6} lg={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
       </Grid>
