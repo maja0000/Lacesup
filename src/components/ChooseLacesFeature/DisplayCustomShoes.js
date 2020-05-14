0f93b06b4fa9061ab037f779fa5403f4e286e008
Elena <anel92r@gmail.com>
2020-05-14 13:57:13 +0200

    ignore shoes without color variants

--- src/components/ChooseLacesFeature/DisplayCustomShoes.js
+++ src/components/ChooseLacesFeature/DisplayCustomShoes.js
@@ -1,5 +1,5 @@
-import ChangeLaces from './ChangeLaces';
-import React, { useState, useEffect } from 'react';
+import ChangeLaces from "./ChangeLaces";
+import React, { useState, useEffect } from "react";
 
 function DisplayCustomShoes() {
   const [shoes, setShoes] = useState([]);
@@ -8,22 +8,22 @@ function DisplayCustomShoes() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://laceup-backend.herokuapp.com/')
+    fetch("https://laceup-backend.herokuapp.com/")
       .then((res) => res.json())
       .then((res) => {
-        setShoes(res.result);
+        setShoes(res.result.filter((shoe) => shoe.colorVariants.length));
         setLoading(false);
       })
       .catch((error) => {
-        console.log('Error: ', error);
+        console.log("Error: ", error);
       });
-    fetch('https://laceup-backend.herokuapp.com/laces')
+    fetch("https://laceup-backend.herokuapp.com/laces")
       .then((res) => res.json())
       .then((res) => {
         setLaces(res.result);
       })
       .catch((error) => {
-        console.log('Error: ', error);
+        console.log("Error: ", error);
       });
   }, []);
   // console.log('outside', shoes.result);
@@ -33,7 +33,7 @@ function DisplayCustomShoes() {
 
   return (
     <div>
-      {loading ? 'Loading...' : <ChangeLaces laces={laces} shoes={shoes} />}
+      {loading ? "Loading..." : <ChangeLaces laces={laces} shoes={shoes} />}
     </div>
   );
 }
